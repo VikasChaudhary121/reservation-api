eda6bb7335d822d569ce95db68003e62790a82bd
VikasChaudhary121 <vikaschaudhary432005@gmail.com>
2024-06-25 18:14:32 +0530

    installed bycryptjs

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,5 @@
 const Users = require("../models/users");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const createError = require("../utils/erorr");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
